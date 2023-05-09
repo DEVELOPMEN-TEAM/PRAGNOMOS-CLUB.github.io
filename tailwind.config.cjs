7fbaf4b9eee8fed1d165f0a0d25fe055c6f1e146
zakaria-lagouader <zakaria.lagouader@gmail.com>
2023-05-09 20:09:54 +0100

    created Showcase section

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: [
+		"./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+		"./node_modules/flowbite/**/*.js",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				inter: ["Inter", "sans-serif"],
+				lexend: ["Lexend", "sans-serif"],
+			},
+		},
+	},
+	plugins: [require("flowbite/plugin")],
+};
