91e2595b8609d0238602ba66de8545af19a52135
Draginhell <77627747+Draginhell@users.noreply.github.com>
2023-05-09 20:10:03 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// News
+document.addEventListener('DOMContentLoaded', function() {
+    new Splide('.splide', {
+        type: 'slide',
+        omitEnd: true,
+        rewind: true,
+        autoWidth: true,
+        perMove: 1,
+        autoplay: 'play',
+        interval: 3000,
+        speed: 1000,
+        pauseOnHover: true,
+        pauseonFocus: true,
+        gap: '50px',
+        mediaQuery: 'min',
+    }).mount();
+});
+
+// FAQ
+$('.question-and-answer').click(function() {
+    var answer = $(this).find(".answer");
+    var arrow = $(this).find(".question-arrow");
+
+    $('.answer').not(answer).slideUp(300);
+    $('.question-arrow').not(arrow).removeClass('rotate-180');
+
+    if ( answer.first().is( ":hidden" ) ) {
+        answer.slideDown(300);
+    } else {
+        answer.slideUp(300);
+    }
+    arrow.toggleClass("rotate-180");
+});
