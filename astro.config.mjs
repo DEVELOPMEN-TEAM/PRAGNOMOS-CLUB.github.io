97cf8ba23d81446484a10b57d428b01047ffd7f3
abdwalid04 <adil.walid.abdellah@gmail.com>
2023-05-12 18:29:49 +0100

    test hosting1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pragnamos-club.github.io',
+  base: '/pragnamos-club.github.io',
   integrations: [tailwind()]
 });
